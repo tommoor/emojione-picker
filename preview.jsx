cae68b539f99037fbc9364502c21fe0026c75efe
Tom Moor <tom.moor@gmail.com>
2016-01-30 15:24:16 -0800

    Fixed: Preview, added compiled files

--- preview.jsx
+++ preview.jsx
@@ -1,6 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var EmojiPicker = require('./js/picker.jsx');
+var EmojiPicker = require('./src/picker.jsx');
 
 var logChoice = function(emoji) {
   console.log(emoji);
