2c5e91605d40244e105aa4a88cfa7af175b7e93a
Nate Norberg <natenorberg@gmail.com>
2016-04-28 15:06:46 -0600

    Added attribution

--- src/picker.jsx
+++ src/picker.jsx
@@ -9,7 +9,8 @@ var _ = require("underscore");
 var Picker = React.createClass({
     propTypes: {
       search: React.PropTypes.string,
-      onChange: React.PropTypes.func.isRequired
+      onChange: React.PropTypes.func.isRequired,
+      showAttribution: React.PropTypes.bool,
     },
 
     getDefaultProps: function() {
@@ -224,6 +225,12 @@ var Picker = React.createClass({
           {this.getModifiers()}
           {this.getSearchInput()}
           {this.getEmojis()}
+          {this.props.showAttribution &&
+            <div className="emojione-attribution">
+              <span>Emoji art supplied by </span>
+              <a href="http://emojione.com" target="_blank">Emoji One</a>
+            </div>
+          }
         </div>
       </div>
     }
