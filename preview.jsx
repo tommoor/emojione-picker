2f5974252a3ce72ce219ffa333ff90f04f8ee01c
Surender Lohia <surender@tenmiles.com>
2016-09-29 15:11:21 +0530

    Added option to exclude some emojis.

--- preview.jsx
+++ preview.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import EmojiPicker from './src/picker';
 
+var excludeEmojis = [":slight_smile:", ":upside_down:", ":money_mouth:", ":nerd:", ":hugging:", ":rolling_eyes:", ":thinking:", ":slight_frown:", ":zipper_mouth:", ":thermometer_face:", ":head_bandage:", ":robot:", ":middle_finger:", ":metal:", ":eye:", ":speaking_head:", ":spy:", ":dark_sunglasses:"]
+
 const logChoice = function(emoji) {
   console.log(emoji);
 }
@@ -9,3 +11,5 @@ const logChoice = function(emoji) {
 ReactDOM.render(<EmojiPicker onChange={logChoice} />, document.getElementById('example1'));
 
 ReactDOM.render(<EmojiPicker search={true} onChange={logChoice} />, document.getElementById('example2'));
+
+ReactDOM.render(<EmojiPicker onChange={logChoice} excludeEmojis={excludeEmojis} />, document.getElementById('example3'));
