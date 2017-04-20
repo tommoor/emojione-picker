be6ce2e76805f1cfcf9d608b07006e7d19cb15b2
Tom Moor <tom.moor@gmail.com>
2017-04-19 22:09:18 -0700

    Closes #66 - Ensure shortname is searchable

--- src/utils/createEmojisFromStrategy.js
+++ src/utils/createEmojisFromStrategy.js
@@ -13,6 +13,11 @@ export default function createEmojisFromStrategy(strategy) {
       const match = key.match(/(.*?)_tone(.*?)$/);
 
       if (match) {
+        // ensure the shortname is included as a keyword
+        if (!value.keywords.includes(match[1])) {
+          value.keywords.push(match[1]);
+        }
+
         // this check is to stop the plugin from failing in the case that the
         // emoji strategy miscategorizes tones - which was the case here:
         // https://github.com/Ranks/emojione/pull/330
@@ -21,6 +26,11 @@ export default function createEmojisFromStrategy(strategy) {
           emojis[value.category][match[1]][match[2]] = value;
         }
       } else {
+        // ensure the shortname is included as a keyword
+        if (!value.keywords.includes(key)) {
+          value.keywords.push(key);
+        }
+
         emojis[value.category][key] = [value];
       }
     }
